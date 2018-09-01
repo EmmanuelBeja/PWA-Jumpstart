4adc4f3e54dc32a802a880f269adaa13abefaf67
EmmanuelBeja <beja.emmanuel@gmail.com>
2018-09-01 09:29:05 +0300

    contains normal project

---
+++ accounts/home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Home page</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/fontawesome-free-5.1.0-web/css/all.css">
+
+  </head>
+  <body>
+
+    <nav>
+      <div class="navbar navbar-default">
+        <ul>
+          <li><a href="../index.php">Index</a></li>
+          <li><a href="home.php">Home</a></li>
+          <li><a href="profile.php">Profile</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="row" style="margin-top:50px;">
+      <div class="col-md-8 offset-md-2">
+        <h1 style="text-align:center">
+          <i class="fa fa-balance-scale" style="color:teal;"></i>
+          <br>
+          home
+        </h1>
+      </div>
+      <div class="col-md-8 offset-md-2 jumbotron">
+      </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="../assets/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="../assets/js/popper.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Progressive Web App!</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-free-5.1.0-web/css/all.css">
+
+  </head>
+  <body>
+
+    <nav>
+      <div class="navbar navbar-default">
+        <ul>
+          <li><a href="index.php">Index</a></li>
+          <li><a href="accounts/home.php">Home</a></li>
+          <li><a href="accounts/profile.php">Profile</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="row" style="margin-top:50px;">
+      <div class="col-md-8 offset-md-2">
+        <h1 style="text-align:center">
+          <i class="fa fa-balance-scale" style="color:teal;"></i>
+          <br>
+          Progressive Web Application
+        </h1>
+
+      </div>
+      <div class="col-md-8 offset-md-2 jumbotron">
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br>
+         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br>
+         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+
+  </body>
+</html>
